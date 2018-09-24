403b98f758c01ced0d1b2266105124f006eb26a2
Skef Iterum <skef@skef.org>
2018-09-23 22:52:05 -0700

    Initial configuration

---
+++ setup.py
@@ -0,0 +1,9 @@
+setup(
+    name='fontforge_coverage_tools',
+    description='Utilities to aid high-glyph-count FontForge projects',
+    url='http://github.com/untenable/fontforge_coverage_tools',
+    author='Skef Iterum',
+    author_email='github@skef.org',
+    license='Modified BSD',
+    scripts=['bin/ff_sfd_pseudoenc']
+)
